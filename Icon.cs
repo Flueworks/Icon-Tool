1d705ea0eeb889e3a73a21002e9254ccc1b7824b
Aksel Kvitberg <akv@locus.no>
2019-12-17 13:55:03 +0100

    Initial commit - create icon from folder

---
+++ Icon.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IconUtil
+{
+    class Icon
+    {
+        public short Reserved { get; private set; } = 0; // Must always be 0
+        public short Type { get; set; } = 1; // 1 = .ico, 2 = .cur
+        public short Count { get; set; }
+
+        public List<Image> Images { get; set; } = new List<Image>();
+
+        public void Read(Stream stream)
+        {
+            BinaryReader reader = new BinaryReader(stream);
+
+            Reserved = reader.ReadInt16();
+            Type = reader.ReadInt16();
+            Count = reader.ReadInt16();
+
+            Images = new List<Image>(Count);
+
+            for (int j = 0; j < Count; j++)
+            {
+                Image img = new Image();
+                img.Read(stream);
+                Images.Add(img);
+            }
+
+        }
+
+        public void Write(Stream stream)
+        {
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            writer.Write(Reserved);
+            writer.Write(Type);
+            writer.Write(Count);
+
+            var offset = 6 + (16 * Count);
+
+            foreach (var image in Images)
+            {
+                image.Offset = offset;
+                image.Write(stream);
+                offset += image.Size;
+            }
+
+            foreach (var image in Images)
+            {
+                writer.Write(image.ImageData, 0, image.Size);
+            }
+        }
+    }
+
+    class Image
+    {
+
+        public byte Height { get; set; }
+        public byte Width { get; set; }
+        public byte Colors { get; set; }
+        public byte Reserved { get; private set; } = 0; // should be 0
+
+        /// <summary>
+        /// In ICO format: Specifies color planes. Should be 0 or 1.
+        /// In CUR format: Specifies the horizontal coordinates of the hotspot in number of pixels from the left.
+        /// </summary>
+        public short Planes { get; set; }
+        /// <summary>
+        /// In ICO format: Specifies bits per pixel. If the images are png, this data is stored in the png data
+        /// In CUR format: Specifies the vertical coordinates of the hotspot in number of pixels from the top.
+        /// </summary>
+        public short BitPerPixel { get; set; }
+        public int Size { get; set; }
+        public int Offset { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public void Read(Stream s)
+        {
+            BinaryReader reader = new BinaryReader(s);
+            Height = reader.ReadByte();
+            Width = reader.ReadByte();
+            Colors = reader.ReadByte();
+            Reserved = reader.ReadByte();
+            Planes = reader.ReadInt16();
+            BitPerPixel = reader.ReadInt16();
+            Size = reader.ReadInt32();
+            Offset = reader.ReadInt32();
+
+            var position = s.Position;
+
+            ImageData = new byte[Size];
+            s.Seek(Offset, SeekOrigin.Begin);
+            int bytesread = s.Read(ImageData, 0, Size);
+            s.Seek(position, SeekOrigin.Begin);
+        }
+
+        public void Write(Stream s)
+        {
+            BinaryWriter writer = new BinaryWriter(s);
+
+            writer.Write(Height);
+            writer.Write(Width);
+            writer.Write(Colors);
+            writer.Write(Reserved);
+            writer.Write(Planes);
+            writer.Write(BitPerPixel);
+            writer.Write(Size);
+            writer.Write(Offset);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace IconUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usecase 1: Run the tool in a folder to gather all the png files starting with x and combine them into an icon file
+
+            var outputFile = args.FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(outputFile))
+            {
+                outputFile = "icon.ico";
+            }
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var icon = CreateIconFromDirectory(currentDirectory);
+
+            try
+            {
+                using var fileStream = File.OpenWrite(outputFile);
+                icon.Write(fileStream);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Creates an icon from all .png files matching the regex x\d+\.png in the specified directory.
+        /// </summary>
+        /// <param name="directory">Directory containing png files</param>
+        /// <param name="outputFile">The output path for the icon</param>
+        /// <exception cref="IOException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        private static Icon CreateIconFromDirectory(string directory)
+        {
+            var files = Directory.GetFiles(directory, "x*.png");
+
+            var fileRegex = new Regex("x\\d+\\.png");
+
+            List<string> acceptedFiles = new List<string>();
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                if (fileName == null)
+                    continue;
+
+                try
+                {
+                    if (fileRegex.IsMatch(fileName))
+                    {
+                        acceptedFiles.Add(file);
+                    }
+                }
+                catch (RegexMatchTimeoutException)
+                {
+                    continue;
+                }
+            }
+
+            var icon = GenerateIconFromFiles(acceptedFiles);
+
+            return icon;
+        }
+
+        private static Icon GenerateIconFromFiles(IEnumerable<string> files)
+        {
+            Icon icon = new Icon { Type = 1 };
+
+            foreach (var file in files)
+            {
+                if (!File.Exists(file))
+                {
+                    continue;
+                }
+
+                try
+                {
+                    var image = CreateImageFromFile(file);
+                    icon.Count++;
+                    icon.Images.Add(image);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e.Message);
+                    // skip file
+                }
+            }
+
+            return icon;
+        }
+
+        /// <summary>
+        /// CreateImageFromFile
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="IOException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="ObjectDisposedException"></exception>
+        private static Image CreateImageFromFile(string file)
+        {
+            Image image = new Image
+            {
+                Colors = 0,
+                Planes = 1
+            };
+
+            var stream = File.Open(file, FileMode.Open, FileAccess.ReadWrite);
+            image.Size = (int)stream.Length;
+            image.ImageData = new byte[image.Size];
+            stream.Read(image.ImageData, 0, image.Size);
+            stream.Close();
+
+            Bitmap bitmap = new Bitmap(file);
+            var width = bitmap.Size.Width;
+            var height = bitmap.Size.Height;
+            bitmap.Dispose();
+
+            // 0 means image width is 256 pixels
+            if (width == 256)
+                width = 0;
+            if (height == 256)
+                height = 0;
+
+            // todo: Add checks for width x height
+
+            image.Height = (byte)height;
+            image.Width = (byte)width;
+            return image;
+        }
+    }
+}
